e2510528f8db97ebdbce4c6a6cee5b1d9b3b5db1
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2021-10-31 22:05:07 -0700

    Created the abstract tools needed for JS web development, with documentation and a test page.
    Separated the tools from the main js file. The main file will not make use of tools.js, but does not contain the code

--- js/sketch.js
+++ js/sketch.js
@@ -1,50 +1,8 @@
-/***
- * initialize html and body components.
- */
-let init = () => {
-    document.querySelectorAll('html, body'+'').forEach(node => {
-    node.style.cssText =
-            '    width: 100%;\n' +
-            '    height: 100%;\n' +
-            '    margin: 0px;\n' +
-            '    padding: 0px;\n' +
-            '    font-family: \'Zen Maru Gothic\', sans-serif;\n' +
-            '    font-weight: 500;\n' +
-            '    font-size: 20px' ;
-    });
-}
+import {init, modifyContainer, addElement} from "./tools.js";
 
-/***
- * Initialize any element that may be used as container, according to the parameters.
- *
- * @param container the container to be initialized
- * @param displayType set container display type to this parameter
- * @param args arg1: width, arg2: height, arg3: margin, arg4: border, arg5: padding
- */
-let initContainer = (container, displayType, ...args) => {
-
-    container.style.display = displayType;
-    container.style.width = args[0];
-    container.style.height = args[1];
-    container.style.margin = args[2];
-    container.style.border = args[3];
-    container.style.padding = args[4];
-};
-
-/***
- * create element of type 'type', and append it as the child of 'parent'
- *
- * @param type type of element to be created
- * @param parent select the parent of the element
- */
-let addElement = (type, parent) => {
-    let element = document.createElement(type)
-    parent.appendChild(element);
-    return element;
-};
 
 init();
 let container = document.querySelector('.container'+'');
-initContainer(container,'flex', '100%', '100%', '0', 'solid');
-
+modifyContainer(container,'flex', '100%', '100%', '0', 'solid');
+addElement('div', '', container);
 
